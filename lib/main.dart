097fdd430ace91393ac2e8a3af87aeb473af0bdf
caio vellani <caiovellani@caios-MacBook-Air.local>
2023-08-17 23:28:13 -0300

    step-14

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:notepad/constants/routes.dart';
+import 'package:notepad/notes/new_note_view.dart';
 import 'package:notepad/services/auth/auth_service.dart';
 import 'package:notepad/views/login_view.dart';
-import 'package:notepad/views/notes_view.dart';
+import 'package:notepad/notes/notes_view.dart';
 import 'package:notepad/views/register_view.dart';
 import 'package:notepad/views/verify_email_view.dart';
 
@@ -20,6 +21,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
+        newNoteRoute: (context) => const NewNoteView(),
       },
     ),
   );
